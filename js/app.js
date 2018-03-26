da7d7f82ccb8a5dcf159f9da2073c188d22cd986
MarvMan <marvin.kai@gmx.de>
2018-03-26 20:57:48 +0200

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-var $frame = $('#result');
-var doc = $frame[0].contentWindow.document;
-var $body = $(document,doc);
-$body.html("<html><body><h1>Hi</h1></body></html>");
+"use strict";
+var editor;
+
+function transformContent() {
+  var $frame = $('.result');
+  var doc = $frame[0].contentWindow.document;
+  var $body = $('html',doc);
+  $body.html(editor.getValue());
+}
+
+require.config({ paths: { 'vs': 'lib/monaco/min/vs' }});
+	require(['vs/editor/editor.main'], function() {
+		editor = monaco.editor.create(document.getElementById('code'), {
+			value: [
+				'<!DOCTYPE html>',
+        '<html>',
+        '<head>',
+        ' <meta charset="utf-8">',
+        '</head>',
+        '<body>',
+        ' ',
+        '</body>',
+        '</html>'
+			].join('\n'),
+			language: 'html',
+      scrollBeyondLastLine: false,
+      theme: 'vs-dark'
+		});
+    setInterval(transformContent, 1);
+	});
