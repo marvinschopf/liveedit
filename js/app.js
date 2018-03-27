db569a659bc05b2ce009641abc5bbac990796a59
MarvMan <marvin.kai@gmx.de>
2018-03-27 13:28:44 +0200

    async

--- js/app.js
+++ js/app.js
@@ -9,23 +9,19 @@ function transformContent() {
   Cookies.set('content', editor.getValue());
 }
 
+function getStartupValue() {
+
+  return '<!DOCTYPE html>\n<html>\n<head>\n <meta charset="utf-8">\n</head>\n<body>\n \n</body>\n</html>';
+
+}
+
 require.config({ paths: { 'vs': 'lib/monaco/min/vs' }});
 	require(['vs/editor/editor.main'], function() {
 		editor = monaco.editor.create(document.getElementById('code'), {
-			value: [
-				'<!DOCTYPE html>',
-        '<html>',
-        '<head>',
-        ' <meta charset="utf-8">',
-        '</head>',
-        '<body>',
-        ' ',
-        '</body>',
-        '</html>'
-			].join('\n'),
+			value: getStartupValue(),
 			language: 'html',
       scrollBeyondLastLine: false,
       theme: 'vs-dark'
 		});
-    setInterval(transformContent, 1);
+    
 	});
